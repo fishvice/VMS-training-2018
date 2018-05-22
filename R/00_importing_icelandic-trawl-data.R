7b3cb81c84f72ae5754fb54f858e9e64de7aec6a
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2018-05-22 16:48:58 +0000

    shiny refinements

--- R/00_importing_icelandic-trawl-data.R
+++ R/00_importing_icelandic-trawl-data.R
@@ -2,7 +2,7 @@ library(mar)
 con <- connect_mar()
 VID <-
   afli_stofn(con) %>%
-  filter(ar %in% 2009:2017,
+  filter(ar %in% 2017,
          veidarf == 6) %>%
   collect(n = Inf) %>%
   pull(skipnr) %>%
@@ -14,7 +14,7 @@ tbl_mar(con, "stk.stk_vms_v") %>%
          lat = poslat * 45 / atan(1),
          heading = heading * 45 / atan(1),
          speed = speed * 1.852) %>%
-  filter(year %in% c(2009:2017),
+  filter(year %in% c(2017),
          between(lon, -28, -18),
          between(lat, 65.5, 67.5),
          skip_nr %in% VID) %>%
